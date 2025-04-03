40a3e64ec365a70f3dca6bc8d826e7b15622907e
Joaofelipe14 <jaofelpe14@gmail.com>
2025-04-03 19:45:44 -0300

    feat: :rocket: Fazendo tela de login
    Fazendo tela de login
    
    Fazendo tela de login

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,19 +1,41 @@
 <template>
-    <div>
-      <h1>Login</h1>
+  <section class="container">
+    <div class="card">
+
+      <h2> Login </h2>
+
+      <button class="btn-principal ">
+        Fazer login com a Huggy
+      </button>
+      
     </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    name: 'Login',
-  };
-  </script>
-  
-  <style scoped>
-  h1 {
-    text-align: center;
-    margin-top: 50px;
-  }
-  </style>
-  
+  </section>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Login',
+};
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  height: 100vh;
+}
+
+.card {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 24px;
+
+
+}
+</style>
